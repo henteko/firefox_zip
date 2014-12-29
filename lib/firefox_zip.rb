8056addf6d7877358839c4aadcb4e37d133f9ecc
henteko <henteko07@gmail.com>
2014-12-29 22:45:48 +0900

    できたっぽい

--- lib/firefox_zip.rb
+++ lib/firefox_zip.rb
@@ -3,11 +3,18 @@ require 'firefox_zip/files/project'
 
 module FirefoxZip
   class << self
-    attr_reader :name
+    attr_reader :name, :description, :launch_path, :icons, :icon, :type, :permissions
 
     def analyze(file_path)
-      @name = 'hello'
-      
+      project = Files::Project.new(file_path)
+      @name = project.manifest_data.name
+      @description = project.manifest_data.description
+      @launch_path = project.manifest_data.launch_path
+      @icons = project.icons
+      @icon = project.icon
+      @type = project.manifest_data.type
+      @permissions = project.manifest_data.permissions
+
       self
     end
   end
--- spec/firefox_zip_spec.rb
+++ spec/firefox_zip_spec.rb
@@ -2,10 +2,40 @@ require 'spec_helper'
 
 describe FirefoxZip do
   context 'self.analyze' do
-    it 'default' do
-      app = FirefoxZip.analyze(File.expand_path('../test_files/hello.zip', __FILE__))
-      
-      expect(app.name).to eq 'hello'
+    before do
+      @app = FirefoxZip.analyze(File.expand_path('../test_files/hello.zip', __FILE__))
+    end
+    
+    it 'get name' do
+      expect(@app.name).to eq 'hello'
+    end
+
+    it 'get description' do
+      expect(@app.description).to eq 'A Hello World app'
+    end
+
+    it 'get launch_path' do
+      expect(@app.launch_path).to eq '/index.html'
+    end
+
+    it 'get icons' do
+      expect(@app.icons).not_to be_nil
+    end
+
+    it 'get icon' do
+      expect(@app.icon).not_to be_nil
+    end
+
+    it 'get type' do
+      expect(@app.type).to eq 'privileged'
+    end
+
+    it 'get permissions' do
+      expect(@app.permissions).to match []
+    end
+    
+    it 'raise' do
+      expect{ FirefoxZip.analyze('') }.to raise_error
     end
   end
 end
