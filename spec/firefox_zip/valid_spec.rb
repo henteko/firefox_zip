583e4772f7b9628db0456760fe264ab95a20a10e
henteko <henteko07@gmail.com>
2015-01-09 22:37:41 +0900

    app-validatorがない環境でテスト落ちるのを修正

--- spec/firefox_zip/valid_spec.rb
+++ spec/firefox_zip/valid_spec.rb
@@ -2,12 +2,16 @@ require 'spec_helper'
 
 describe FirefoxZip::Valid do
   context 'self.run' do
+    before do
+      @status, stdout, stderr = systemu 'app-validator --help'
+    end
+
     it 'success' do
-      expect { FirefoxZip::Valid.run(File.expand_path('../../test_files/package.zip', __FILE__)) }.not_to raise_error
+      expect { FirefoxZip::Valid.run(File.expand_path('../../test_files/package.zip', __FILE__)) }.not_to raise_error if @status.exitstatus == 0
     end
 
     it 'error' do
-      expect { FirefoxZip::Valid.run(File.expand_path('../../test_files/error_package.zip', __FILE__)) }.to raise_error
+      expect { FirefoxZip::Valid.run(File.expand_path('../../test_files/error_package.zip', __FILE__)) }.to raise_error if @status.exitstatus == 0
     end
   end
 
--- spec/firefox_zip_spec.rb
+++ spec/firefox_zip_spec.rb
@@ -72,12 +72,16 @@ describe FirefoxZip do
   end
 
   context 'self.analyze valid false' do
+    before do
+      @status, stdout, stderr = systemu 'app-validator --help'
+    end
+
     it 'not raise error' do
-      expect { FirefoxZip.analyze(File.expand_path('../test_files/error_package.zip', __FILE__)) }.not_to raise_error
+      expect { FirefoxZip.analyze(File.expand_path('../test_files/error_package.zip', __FILE__)) }.not_to raise_error if @status.exitstatus == 0
     end
 
     it 'raise error' do
-      expect { FirefoxZip.analyze(File.expand_path('../test_files/error_package.zip', __FILE__), valid: true) }.to raise_error
+      expect { FirefoxZip.analyze(File.expand_path('../test_files/error_package.zip', __FILE__), valid: true) }.to raise_error if @status.exitstatus == 0
     end
   end
 end
